08b4d47e11c3d74c95351ac7322208a0d235c7d2
Cactiw <Knyazev1514@gmail.com>
2022-01-27 23:53:20 +0300

    Changed internal errors inheritance

--- python_socks/_errors.py
+++ python_socks/_errors.py
@@ -1,12 +1,12 @@
-class ProxyError(Exception):
+class ProxyError(IOError):
     def __init__(self, message, error_code=None):
         super().__init__(message)
         self.error_code = error_code
 
 
-class ProxyTimeoutError(TimeoutError):
+class ProxyTimeoutError(IOError):
     pass
 
 
-class ProxyConnectionError(OSError):
+class ProxyConnectionError(IOError):
     pass
